4ed430bd65e2d80a2fbfc156e2f089aad95de55e
0xFlaps4 <36132045+Flaps4@users.noreply.github.com>
2025-06-04 10:52:49 +0200

    Fix f-string for sum output in LoopLab3

--- LoopLab3.py
+++ LoopLab3.py
@@ -3,4 +3,4 @@ while True:
     tal = int(input("Mata in ett tal"))
     print(f"Du matade in {tal}")
     summa = summa + tal
-    print("Summan av alla tal är {summa}")
+    print(f"Summan av alla tal är {summa}")
